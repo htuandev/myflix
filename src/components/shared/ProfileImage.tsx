09363162577fc623ef531f0499373933c18e9a26
htuandev <130202652+htuandev@users.noreply.github.com>
2024-03-22 09:00:14 +0700

    Profile Image

---
+++ src/components/shared/ProfileImage.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import Image from 'next/image';
+import { TMDB_IMAGE_SIZES, Gender, BASE64_FEMALE, BASE64_MALE } from '@/constants';
+import { cn, tmdbImageSrc } from '@/lib/utils';
+
+type Props = {
+  src: string | undefined;
+  gender: Gender;
+  alt?: string;
+  className?: string;
+  style?: React.CSSProperties;
+  size?: 'sm' | 'md' | 'lg';
+};
+
+const ProfileImage = ({ src, gender, alt, className, style, size = 'sm' }: Props) => {
+  const imageSize = TMDB_IMAGE_SIZES.face[size];
+
+  const defaultUrl = gender === Gender.Female ? BASE64_FEMALE : BASE64_MALE;
+
+  return (
+    <Image
+      className={cn(' pointer-events-none aspect-square select-none overflow-hidden rounded-full', className)}
+      style={style}
+      src={tmdbImageSrc(src, imageSize)}
+      alt={alt || 'No image'}
+      priority
+      width={size === 'sm' ? 235 : size === 'md' ? 375 : 470}
+      height={size === 'sm' ? 235 : size === 'md' ? 375 : 470}
+      placeholder='blur'
+      blurDataURL={defaultUrl}
+      onError={(e) => (e.currentTarget.src = defaultUrl)}
+    />
+  );
+};
+
+export default ProfileImage;
